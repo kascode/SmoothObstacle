ea9d4f498403bff4f7d0041ce2f6b9ab8e1a15b8
Nikita Kasyanov <nik.kasyanov@gmail.com>
2016-02-13 14:28:36 +0300

    empty db config

---
+++ config/db.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "development": {
+    // Enter your db parameters
+    "database": "",
+    "username": "",
+    "password": "",
+    "config": {
+      "host": "localhost",
+      "dialect": "mysql"
+    }
+  }
+};
